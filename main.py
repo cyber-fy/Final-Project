dd729d742aeaf6f08e056f400a16537b9429682f
cyber-fy <farah_yahaya@brown.edu>
2023-05-03 16:54:59 -0400

    test 2

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print("hello world")
     print("hey mamas")
+    print("test 2")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
