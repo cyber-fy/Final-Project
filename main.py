3d9283f17ef9ff5fa4f43e2798242f677201c0c7
cyber-fy <farah_yahaya@brown.edu>
2023-05-04 16:07:15 -0400

    testing

--- main.py
+++ main.py
@@ -2,20 +2,6 @@
 import pygame
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print("hello world")
-    print("hey mamas")
-    print("test 2")
-
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 ## PUSH
@@ -25,3 +11,21 @@ if __name__ == '__main__':
 
 ## PULL
 ## 1. git pull
+
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((600, 900))
+test_surface = pygame.Surface((100, 200))
+test_surface.fill('Red')
+board_surface= pygame.image.load('backgroundnew.jpeg').convert()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    screen.blit(board_surface,(0,0))
+    pygame.display.update()
+
